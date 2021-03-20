ae5a773b42988a0d546bbdff7becfcd59bf4f795
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-03-20 14:14:07 -0500

    commit tree 06f8378339744120b569ef801c9b7a1a48660406

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function focus() {
+        console.log('Good')
+    }
+
+    function blur() {
+        console.log('Hey! Eyes on me!')
+    }
+
+    return (
+        <div>
+            <button onFocus={focus} onBlur={blur}>Eyes On Me</button>
+
+        </div>
+    )
+}
+
+
+
+export default EyesOnMe;
